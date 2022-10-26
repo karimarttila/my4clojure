f33f5d3587a1038fff9cce1b00cd855926e060b0
Kari Marttila <kari.marttila@iki.fi>
2022-10-26 20:50:07 +0300

    Check status babashka script

--- scratch/misc1.clj
+++ scratch/misc1.clj
@@ -40,3 +40,33 @@ misc1/jee2
      (map name)
      (filter #(clojure.string/ends-with? % "misc1")))
 
+(def status (slurp "misc/status_2022-10-26.csv"))
+status
+(def lines (drop 1 (str/split-lines status)))
+lines
+(take 10 lines)
+(take 10 (-> lines reverse))
+(def items (map (fn [line]
+                  (str/split line #";"))
+                lines))
+items
+(count items)
+(def ctgr (fn [line] (get line 2)))
+(ctgr (first items))
+(ctgr (first (drop 10 items)))
+(map ctgr items)
+(def grouped (group-by ctgr items))
+(map (fn [[k v]]
+       (let [solved (filter (fn [item]
+                              (let [solved? (get item 3)]
+                                (str/includes? solved? "Passed"))
+                              ) v)]
+         {k {:count (count v) :solved (count solved) :percentage (int (* 100 (double (/ (count solved) (count v)))))}}))
+     grouped)
+
+
+
+
+
+
+
---
+++ src/clj/check_status.clj
@@ -0,0 +1,40 @@
+#!/usr/bin/env bb
+
+(require '[clojure.string :as str])
+(require '[clojure.java.io :as io])
+(require '[clojure.pprint :as pp])
+
+
+(def process-file
+  (fn [file]
+    (let [lines (drop 1 (str/split-lines (slurp file)))
+          items (map (fn [line]
+                       (str/split line #";"))
+                     lines)
+          ; Categories (easy, elementary... column)
+          ctgr (fn [line] (get line 2))
+          grouped (group-by ctgr items)]
+      (println (str "Starting to process file: " file))
+      (reduce (fn [acc [k v]]
+                (let [solved (filter (fn [item]
+                                       (let [solved? (get item 3)]
+                                         (str/includes? solved? "Passed"))) v)]
+                  (assoc acc k {:count (count v)
+                                :solved (count solved)
+                                :percentage (int (* 100 (double (/ (count solved) (count v)))))})))
+              {}
+              grouped))))
+
+; When debugging in Clojure JVM REPL.
+;(pp/pprint (process-file "misc/status_2022-10-26.csv"))
+
+; Run with babashka.
+(let [[file] *command-line-args*]
+  (when (empty? file)
+    (println "Usage: <file>")
+    (System/exit 1))
+  (when (not (.exists (io/file file)))
+    (println (str "File does not exist: " file))
+    (System/exit 1))
+  (pp/pprint (process-file file)))
+
